794602bb3da650940115076042c76c25539027f4
err-cho404 <snowleo456@gmail.com>
2024-08-11 04:56:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 7.37 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(numer1, denom1, numer2, denom2) {
+    var answer = [];
+    if(0 <numer1< 1000 && 0 <denom1< 1000 && 0 <numer2< 1000 && 0 <denom2< 1000){
+        answer[0]=(numer1*denom2)+(numer2*denom1);
+        answer[1]=denom1*denom2;
+        for(i=(denom1*denom2);i>=2;i--){
+            if(answer[0]%i==0 && answer[1]%i==0){
+                answer[0]=answer[0]/i;
+                answer[1]=answer[1]/i;
+            }
+        }
+    }
+    return answer;
+}
