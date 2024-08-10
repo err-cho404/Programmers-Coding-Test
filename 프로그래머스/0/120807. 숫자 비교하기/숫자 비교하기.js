cfd076f89729cab0d8ed4774d842e12188f3cbdd
err-cho404 <snowleo456@gmail.com>
2024-08-11 03:12:05 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,11 +1,7 @@
 function solution(num1, num2) {
     var answer = 0;
     if(0 <= num1 && num1 <= 10000 && 0 <= num2 && num2 <= 10000){
-        if(num1==num2){
-            answer= 1
-        }else{
-            answer= -1
-        }
+        answer = num1==num2? 1 : -1
     }
     return answer;
 }
