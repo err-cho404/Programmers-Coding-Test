fd775504b32eb0cf5939d5b0e857f6c17d42eb3d
err-cho404 <snowleo456@gmail.com>
2024-08-13 00:02:31 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    pizza=6
+    if 1 <= n <= 100:
+        if n==6 or n==1:
+            answer=1
+        else:
+            for i in range(0,n-1):
+                if ((pizza+(i*6))%n)==0:
+                    answer=(pizza+(i*6))//6
+                    break
+                else:
+                    answer=n
+    return answer
