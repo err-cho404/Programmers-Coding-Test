d51d20f13109315491039ce7fad279d95de9d2de
err-cho404 <snowleo456@gmail.com>
2024-08-15 03:00:12 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,9 @@
+function solution(age) {
+    var answer = 0;
+    var result=0;
+    if(0<age && age<=120){
+        let standYear=2022;
+        answer=standYear-(age-1);
+    }
+    return answer;
+}
