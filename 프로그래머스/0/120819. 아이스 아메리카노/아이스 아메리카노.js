2c7df3072eaf5e5c3d89230f9a707464d9c702bd
err-cho404 <snowleo456@gmail.com>
2024-08-15 02:50:44 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -2,8 +2,8 @@ function solution(money) {
     var answer = [];
     if(0 < money && money<= 1000000){
         var ame=5500
-        answer[0]=Math.floor(money/ame);
-        answer[1]=money%ame;
+        answer.push(Math.floor(money/ame));
+        answer.push(money%ame);
     }
     return answer;
 }
