2f6c49c68e177c917b404309b77d1e9465e45757
err-cho404 <snowleo456@gmail.com>
2024-08-15 04:06:51 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,10 @@
 function solution(my_string) {
     var answer = '';
+    var x=my_string.split('').reverse();
     if(1<=my_string.length && my_string.length<=1000){
-       answer=[...my_string].reverse().join(''); 
+        for(i=0;i<x.length;i++){
+            answer+=x[i];
+        }
     }
     return answer;
 }
