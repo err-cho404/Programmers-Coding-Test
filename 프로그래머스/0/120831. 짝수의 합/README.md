fa515285d8e4a22dae69eb5db3121869560405b9
err-cho404 <snowleo456@gmail.com>
2024-08-18 02:21:46 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/README.md
+++ 프로그래머스/0/120831. 짝수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수의 합 - 120831 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120831) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120831?language=java) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 7.76 ms
+메모리: 72.7 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 18일 02:08:57
+2024년 08월 18일 02:21:45
 
 ### 문제 설명
 
--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,14 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         if(0 < n && n <= 1000){
-            int x=0;
-            int result=n-(n%2);
-            for(int i=1;i<(n/2);i++){
-                x=2*i;
-                result=result+x;
-                System.out.println(result);
+            n=n-(n%2);
+            int result=n;
+            for(int i=2;i<n;i+=2){
+                result=result+i;
             }
-            return answer=result;
+            return answer=result; 
         }
         return answer;
     }
