f101f19863cff1850a7528b35a2d607754dc66d3
err-cho404 <snowleo456@gmail.com>
2024-08-20 15:40:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 4.79 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,16 +1,7 @@
 function solution(emergency) {
-    var answer = [];
-    var arr_emergency=[];
-    var dict={};
-    for(i=0;i<emergency.length;i++){
-        arr_emergency.push(emergency[i]);
-    } //원본 응급도 저장
-    var sort_list=arr_emergency.sort((a,b)=>a-b); //응급도 순서대로 배열
-    for(i=emergency.length;i>0;i--){
-        dict[sort_list[emergency.length-i]]=i;
-    } //딕셔너리에 응급도:응급도 순서 로 정리
-    for(j=0;j<emergency.length;j++){
-        answer.push(dict[`${emergency[j]}`]);
-    } 
-    return answer;
+    let sorted = emergency.slice().sort((a,b)=>b-a);
+    console.log(sorted);
+    return emergency.map(v=>sorted.indexOf(v)+1);//sorted의 인덱스를 emergency에 map으로 적용
 }
+
+
