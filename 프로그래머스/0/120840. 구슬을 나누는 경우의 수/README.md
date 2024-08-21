23d183465c547a3e7b480b6660455325ec9be7da
err-cho404 <snowleo456@gmail.com>
2024-08-22 01:44:43 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.27 ms
+메모리: 33.5 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 22일 01:31:03
+2024년 08월 22일 01:44:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,29 +1,16 @@
 function solution(balls, share) {
     var answer = 0;
     if(1 <= balls && balls<= 30 && share <= balls && 1 <= share && share<= 30 ){
-        var mulshare=[];
-        var mulballs=[];
-        var mulball=[];
-        var x=1;
-        var y=1;
-        var z=1;
-        var ball= balls-share;
-        if(ball == 0){
+        var result=1;
+        if(balls-share == 0){
             answer=1;
         }else{
-            for(i=0;i<share;i++){
-                mulshare.push(share-(1*i));
-                x *= mulshare[i];
+            while(share > 0){
+                result = result * balls / share;
+                balls = balls-1;
+                share = share-1;
             }
-            for(i=0;i<balls;i++){
-                mulballs.push(balls-(1*i));
-                y *= mulballs[i];
-            }
-            for(i=0;i<ball;i++){
-                mulball.push(ball-(1*i));
-                z *= mulball[i];
-            }
-            answer=Math.round(y/(z*x));
+            answer=Math.round(result);
         }
     }
     return answer;
