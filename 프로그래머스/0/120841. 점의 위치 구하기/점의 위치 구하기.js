74b416c1ca88b3596f515e4095d558733537469b
err-cho404 <snowleo456@gmail.com>
2024-08-22 19:33:55 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,15 @@
+function solution(dot) {
+    var answer = 0;
+    if (dot.length==2 && -500 <= dot[0] && dot[0] <= 500&&-500 <= dot[1] && dot[1] <= 500 && dot[0]!=0 && dot[1]!=0){
+        if(dot[0]>0&&dot[1]>0){
+            answer=1;
+        }else if(dot[0]<0&&dot[1]>0){
+            answer=2;
+        }else if(dot[0]<0&&dot[1]<0){
+            answer=3;
+        }else{
+            answer=4;
+        }
+    }
+    return answer;
+}
