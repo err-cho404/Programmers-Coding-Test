64471f1cf5cd2d76aefa86ea72addc958ce3342c
err-cho404 <snowleo456@gmail.com>
2024-08-24 22:58:42 +0900

    [level 0] Title: 공 던지기, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,9 +1,8 @@
 function solution(numbers, k) {
     var answer = 0;
     if(2<numbers.length && numbers.length<100 && 0<k && k<1000){
-        var x=numbers.length*k;
         var idx=0;
-        for(j=0;j<x;j++){
+        for(j=0;j<(k*2);j++){
             numbers.push(numbers[j]);
         }
         for(i=1;i<=k;i++){
