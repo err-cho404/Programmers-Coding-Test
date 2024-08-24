0f63cfabbab13fef7816997d76c9a1c4cf095d47
err-cho404 <snowleo456@gmail.com>
2024-08-24 23:00:21 +0900

    [level 0] Title: 공 던지기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/README.md
+++ 프로그래머스/0/120843. 공 던지기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 공 던지기 - 120843 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120843) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.49 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 24일 22:58:40
+2024년 08월 24일 23:00:19
 
 ### 문제 설명
 
--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,14 +1,7 @@
 function solution(numbers, k) {
     var answer = 0;
     if(2<numbers.length && numbers.length<100 && 0<k && k<1000){
-        var idx=0;
-        for(j=0;j<(k*2);j++){
-            numbers.push(numbers[j]);
-        }
-        for(i=1;i<=k;i++){
-            answer=numbers[idx];
-            idx=idx+2;
-        }
+        return numbers[((2 * (k -1))) % numbers.length];
     }
     return answer;
 }
