7ea5ce379a7ab7518846f26fe8c58e64232d05a8
err-cho404 <snowleo456@gmail.com>
2024-08-24 23:46:29 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,18 +1,12 @@
 function solution(numbers, direction) {
     var answer = [];
-    var result=[]
+    var result = [...numbers];
     if(3<=numbers.length<=20){
         if(direction=="right"){
-            result.push(numbers[numbers.length-1]);
-            for(i=0;i<numbers.length-1;i++){
-                result.push(numbers[i]);
-            }
+            result.unshift(result.pop());
             answer=result;
         }else if(direction=="left"){
-            for(i=1;i<numbers.length;i++){
-                result.push(numbers[i]);
-            }
-            result.push(numbers[0]);
+            result.push(result.shift());
             answer=result;
         }
     }
