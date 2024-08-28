de51061c76b68c3e0992af8b5017de7d53ce5658
err-cho404 <snowleo456@gmail.com>
2024-08-29 00:54:51 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -2,18 +2,11 @@ function solution(s) {
     var answer = 0;
     var arr=s.split(' ');
     var numarr=arr.filter(idx=>idx!='Z').map(Number);
-    var sum=numarr.reduce((a,b)=>a+b);
-    var zIdxArr=[];
     if(1<=s.length && s.length<=200 && -1000<Math.min(...numarr) && Math.max(...numarr)<1000 && arr[0]!='Z'){
-        var idx=arr.indexOf('Z',idx);
-        while(idx!=-1){
-            zIdxArr.push(idx);
-            var idx=arr.indexOf('Z',idx+1);
+        while(arr.indexOf('Z')!=-1){
+            arr.splice(arr.indexOf('Z')-1,2);
         }
-        for(i=0;i<zIdxArr.length;i++){
-            sum=sum-arr[zIdxArr[i]-1];
-        }
-        answer=sum;
+        answer=arr.reduce((a,b)=>(Math.floor(a)+Math.floor(b)),0);
     }
     return answer;
 }
