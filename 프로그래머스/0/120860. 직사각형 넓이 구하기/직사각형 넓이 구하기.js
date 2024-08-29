1e343ea74238ec3b7b65f7b50d102cb6f7873586
err-cho404 <snowleo456@gmail.com>
2024-08-30 01:36:10 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 5.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -0,0 +1,17 @@
+function solution(dots) {
+    var answer = 0;
+    var xArr=[];
+    var yArr=[];
+    for(i=0;i<dots.length;i++){
+        xArr.push(dots[i][0]);
+        yArr.push(dots[i][1]);
+    }
+    xArr=xArr.map(a=>a+256);
+    console.log(xArr);
+    xArr=xArr.sort();
+    yArr=yArr.sort();
+    var x=xArr[xArr.length-1]-xArr[0];
+    var y=yArr[yArr.length-1]-yArr[0];
+    var wide=Math.floor(x*y);
+    return answer=wide;
+}
