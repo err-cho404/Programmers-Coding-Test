d95229cbc90072683dfed52789d087aed1372294
err-cho404 <snowleo456@gmail.com>
2024-09-01 01:11:10 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.78 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 01일 00:46:36
+2024년 09월 01일 01:11:09
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,14 +1,8 @@
 function solution(numbers) {
     var answer = 0;
     if(-10000<=numbers[0]&&numbers[numbers.length-1]<=10000&&2<=numbers.length&&numbers.length<=100){
-        var newNum=[];
-        for(i=0;i<numbers.length;i++){
-            for(j=i+1;j<numbers.length;j++){
-                newNum.push(numbers[i]*numbers[j]);
-            }
-        }
-        newNum.sort((a,b)=>b-a);
-        answer=newNum[0];
+        numbers.sort((a,b)=>b-a);
+        answer=Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]);
     }
     return answer;
 }
