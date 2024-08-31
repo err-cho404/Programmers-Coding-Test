b3d975214b033e8af0ec6ed3d07fc26ceec7f787
err-cho404 <snowleo456@gmail.com>
2024-09-01 00:09:28 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/README.md
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 직사각형 넓이 구하기 - 120860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120860?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120860) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.12 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 30일 01:41:52
+2024년 09월 01일 00:09:27
 
 ### 문제 설명
 
--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -6,10 +6,8 @@ function solution(dots) {
         xArr.push(dots[i][0]);
         yArr.push(dots[i][1]);
     }
-    xArr=xArr.map(a=>a+255);
-    yArr=yArr.map(b=>b+255);
-    xArr=xArr.sort();
-    yArr=yArr.sort();
+    xArr=xArr.map(a=>a+255).sort();
+    yArr=yArr.map(b=>b+255).sort();
     var x=xArr[xArr.length-1]-xArr[0];
     var y=yArr[yArr.length-1]-yArr[0];
     var wide=Math.floor(x*y);
