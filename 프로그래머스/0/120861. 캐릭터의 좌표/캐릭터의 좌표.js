a149dd773bdfe0f6a7659f99c1d288c9cea39094
err-cho404 <snowleo456@gmail.com>
2024-08-31 23:43:12 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,36 @@
+function solution(keyinput, board) {
+    var answer = [];
+    var startpoint=[0,0];
+    let [width,length]=startpoint;
+    if(0<=keyinput.length && keyinput.length<=50 && board[0]%2==1 && 1<=board[0] && board[0] <=99 && board[1]%2==1 && 1<=board[1] && board[1] <=99 ){
+        for(i=0;i<keyinput.length;i++){
+            if(keyinput[i]=="up"){
+                if(-Math.floor(board[1]/2)<=length && length<Math.floor(board[1]/2)){
+                    length=length+1;
+                }else{
+                    continue;
+                }
+            }else if(keyinput[i]=="down"){
+                if(-Math.floor(board[1]/2)<length && length<=Math.floor(board[1]/2)){
+                    length=length-1;
+                }else{
+                    continue;
+                }
+            }else if(keyinput[i]=="left"){
+                if(-Math.floor(board[0]/2)<width && width<=Math.floor(board[0]/2)){
+                    width=width-1;
+                }else{
+                    continue;
+                }
+            }else if(keyinput[i]=="right"){
+                if(-Math.floor(board[0]/2)<=width && width<Math.floor(board[0]/2)){
+                    width=width+1;
+                }else{
+                    continue;
+                }
+            }
+        }
+        answer=[width,length];
+    }
+    return answer;
+}
