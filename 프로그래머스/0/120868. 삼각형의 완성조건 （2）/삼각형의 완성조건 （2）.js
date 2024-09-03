7438bdd3f959bf80564ca3fa1dee4139ac7a1e45
err-cho404 <snowleo456@gmail.com>
2024-09-04 01:58:43 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -4,13 +4,7 @@ function solution(sides) {
     var [x,y] = sides;
     if(sides.length==2&& x%1==0 && y%1==0 && 1<=x && x<=1000 && 1<=y && y<=1000){
         sides.sort((a,b)=>a-b);
-        for(i=sides[1]+1;i<sides[0]+sides[1];i++){
-            longSide.push(i);
-        }
-        for(j=sides[1]-sides[0]+1;j<=sides[1];j++){
-            longSide.push(j);
-        }
-        answer=longSide.length;
+        answer=(sides[1]-(sides[1]-sides[0]))+(sides[0]-1);
     }
     return answer;
-}
+}    
