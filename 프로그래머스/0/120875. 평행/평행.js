6ce4023ac73a430c8c58a3b942b0d0a4a81b5a6a
err-cho404 <snowleo456@gmail.com>
2024-09-13 22:44:53 +0900

    [level 0] Title: 평행, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -1,11 +1,7 @@
 function solution(dots) {
     var answer = 0;
     let [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
-    if((x1-x2)/(y1-y2)==(x3-x4)/(y3-y4)){
-        answer=1;
-    }else if((x1-x3)/(y1-y3)==(x2-x4)/(y2-y4)){
-        answer=1;
-    }else if((x1-x4)/(y1-y4)==(x2-x3)/(y2-y3)){
+    if((x1-x2)/(y1-y2)==(x3-x4)/(y3-y4)||(x1-x3)/(y1-y3)==(x2-x4)/(y2-y4)||(x1-x4)/(y1-y4)==(x2-x3)/(y2-y3)){
         answer=1;
     }
     return answer;
