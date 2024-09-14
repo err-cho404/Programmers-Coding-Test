d43f3fb89baa7ddff1155f22c2c386032540c447
err-cho404 <snowleo456@gmail.com>
2024-09-15 00:06:07 +0900

    [level 0] Title: 유한소수 판별하기, Time: 3.87 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,28 @@
+function solution(a, b) {
+    var answer = 0;
+    for(i=b;i>0;i--){
+        if(a%i==0&&b%i==0){
+            var minA=a/i;
+            var minB=b/i;
+            console.log(minA);
+            console.log(minB);
+            break;
+        }
+    };
+    if((minA/minB)%1==0){
+        answer=1;
+    }
+    var arr=[];
+    for(j=2;j<=minB;j++){
+        if(minB%j==0){
+            arr.push(j);
+            if(j%2==0||j%5==0){
+                answer=1;
+            }else{
+                answer=2;
+                break;
+            }
+        }
+    };
+    return answer;
+}
