c412dfb52a687544ca32fe941f02518248a49343
err-cho404 <snowleo456@gmail.com>
2024-09-16 01:44:12 +0900

    [level 0] Title: 등수 매기기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/README.md
+++ 프로그래머스/0/120882. 등수 매기기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 등수 매기기 - 120882 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.25 ms
+메모리: 33.4 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 16일 01:41:27
+2024년 09월 16일 01:44:13
 
 ### 문제 설명
 
--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,17 +1,7 @@
 function solution(score) {
-    var answer = [];
-    var scoreAvg=score.map(([a,b])=>(a+b)/2);
-    var scoreAvgSort=[...scoreAvg];
-    scoreAvgSort.sort((a,b)=>b-a);
-    var rank = scoreAvgSort.map((x)=>[x,scoreAvgSort.indexOf(x)+1]);
-    var result=[];
-    for(i=0;i<rank.length;i++){
-        for(j=0;j<rank.length;j++){
-            if(scoreAvg[i]==rank[j][0]){
-                result.push(rank[j][1]);
-                break;
-            }
-        }
-    }
-    return answer=result;
+  return score.map((el) => {
+    return (
+      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+    );
+  });
 }
