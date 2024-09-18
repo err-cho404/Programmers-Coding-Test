3963b2ac04771ef3fab96a439860aa42aa579fa3
err-cho404 <snowleo456@gmail.com>
2024-09-18 21:27:51 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,15 +1,4 @@
 function solution(chicken) {
-    var answer = 0;
-    var service=[];
-    while (1){
-        var remainCoupon=chicken%10;
-        chicken=Math.floor(chicken/10);
-        service.push(chicken);
-        chicken=chicken+remainCoupon;
-        if(chicken<10){
-            break;
-        }
-    }
-    var result=service.reduce((a,b)=>a+b);
-    return answer=result;
+    var answer = parseInt((chicken-1) / 9);
+    return answer;
 }
