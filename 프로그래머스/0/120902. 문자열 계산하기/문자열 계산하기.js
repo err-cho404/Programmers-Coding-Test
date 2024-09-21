979cae057e2a4682e0b6b9c547e1c870ef09216d
err-cho404 <snowleo456@gmail.com>
2024-09-21 23:31:12 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var result=0;
+    my_string=my_string.split(' ');
+    for(i=1;i<my_string.length;i+=2){
+        if(my_string[i]==='+'){
+                my_string[i+1]=Number(my_string[i-1])+Number(my_string[i+1]);
+        }else{
+            my_string[i+1]=Number(my_string[i-1])-Number(my_string[i+1]);
+        }
+    }
+    return my_string.pop();
+}
