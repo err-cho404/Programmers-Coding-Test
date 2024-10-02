1dbe910f1c2546024c741632c74299f795367a64
err-cho404 <snowleo456@gmail.com>
2024-10-03 00:38:40 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.70 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,5 +1,5 @@
 function solution(n) {
-    var answer = [...Array.from(Array(n),()=>new Array(n).fill(0))];
+    var answer = Array.from(Array(n),()=>new Array(n).fill(0));
     answer.map((x,i)=>x.map((y,j)=>x[i]=1));
     return answer;
 }
