acaf55e808d3e7a51a1d28503d017c5bea3be7d3
err-cho404 <snowleo456@gmail.com>
2024-10-04 20:36:37 +0900

    [level 0] Title: 커피 심부름, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,5 +1,5 @@
 function solution(order) {
     var answer = 0;
-    order.map(x=>x.replace(/ice|hot/g,'')).map(x=>{if(x=='cafelatte'){answer+=5000}else{answer+=4500}});
+    order.map(x=>{if(x.includes('cafelatte')){answer+=5000}else{answer+=4500}});
     return answer;
 }
