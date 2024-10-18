1b49b9eca51ee7139ff17ed07730ab75af0c0cb6
err-cho404 <snowleo456@gmail.com>
2024-10-18 21:14:08 +0900

    [level 0] Title: 세 개의 구분자, Time: 294.48 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,3 +1,3 @@
 function solution(myStr) {
-    return myStr.replace(/[a-c]/g,'').length>0?myStr.split(/[a-c]/).filter(x=>x.length>0):["EMPTY"];
+    return myStr.replace(/[a-c]/g,'')!=['']?myStr.split(/[a-c]/).filter(x=>x.length>0):["EMPTY"];
 }
