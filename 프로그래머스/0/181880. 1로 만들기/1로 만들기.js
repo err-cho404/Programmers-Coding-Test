64d2b2e7b08b557d7c93a0a74bd941455537073f
err-cho404 <snowleo456@gmail.com>
2024-10-23 22:20:51 +0900

    [level 0] Title: 1로 만들기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,15 +1,3 @@
 function solution(num_list) {
-    var answer = 0;
-    for(i=0;i<num_list.length;i++){
-        while(num_list[i]!=1){
-            if(num_list[i]%2==0){
-                num_list.splice(i,1,num_list[i]/2);
-                answer+=1;
-            }else{
-                num_list.splice(i,1,(num_list[i]-1)/2);
-                answer+=1;
-            }
-        }
-    }
-    return answer;
+    return num_list.map(x=>x.toString(2).length-1).reduce((a,b)=>a+b);
 }
--- 프로그래머스/0/181880. 1로 만들기/README.md
+++ 프로그래머스/0/181880. 1로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 1로 만들기 - 181880 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.22 ms
+메모리: 33.5 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 22:10:53
+2024년 10월 23일 22:20:49
 
 ### 문제 설명
 
