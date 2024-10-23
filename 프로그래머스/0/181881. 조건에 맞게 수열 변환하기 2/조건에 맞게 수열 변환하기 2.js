e8549c01a1e13c8c81cd0fb2276608e53c49b998
err-cho404 <snowleo456@gmail.com>
2024-10-23 22:47:03 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 1141.52 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    var count=0;
+    var arrlist=[];
+    while(1){
+        arr=arr.map(x=>(x>=50&&x%2==0)?x/2:(x<50&&x%2!=0)?x*2+1:x);
+        count+=1;
+        arrlist.push(arr);
+        console.log(arrlist);
+        if(arrlist.length==2&&arrlist[arrlist.length-2].toString()===arrlist[arrlist.length-1].toString()){
+            return 0;
+        }else if(arrlist.length>1&&arrlist[arrlist.length-2].toString()===arrlist[arrlist.length-1].toString()){
+            break;
+        }
+    }
+    return count-1;
+}
