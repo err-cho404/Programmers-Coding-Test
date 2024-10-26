30cf79d8193dcc242b7c3687722d3eb926c309ae
err-cho404 <snowleo456@gmail.com>
2024-10-27 00:41:33 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+    if(num_list.filter((_,i)=>i%2!=0).reduce((a,b)=>a+b)>=num_list.filter((_,i)=>i%2==0).reduce((a,b)=>a+b)){
+        return num_list.filter((_,i)=>i%2!=0).reduce((a,b)=>a+b)
+    }else if(num_list.filter((_,i)=>i%2!=0).reduce((a,b)=>a+b)<num_list.filter((_,i)=>i%2==0).reduce((a,b)=>a+b)){
+        return num_list.filter((_,i)=>i%2==0).reduce((a,b)=>a+b)
+    }
+}
