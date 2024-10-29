6c8382e1263d07873cd2801c1f2d051527177772
err-cho404 <snowleo456@gmail.com>
2024-10-29 23:26:55 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,11 @@
+function solution(str_list) {
+    var answer=[];
+    for(i=0;i<str_list.length;i++){
+        if(str_list[i]=='l'){
+            return str_list.slice(0,i);
+        }else if(str_list[i]=='r'){
+            return str_list.slice(i+1);
+        }
+    }
+    return answer;
+}
