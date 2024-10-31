bbf125e7f880e3b23052bceab0be221e96f54b42
err-cho404 <snowleo456@gmail.com>
2024-10-31 23:39:53 +0900

    [level 0] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/README.md
+++ 프로그래머스/0/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.07 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 23:37:47
+2024년 10월 31일 23:39:52
 
 ### 문제 설명
 
--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,3 +1,4 @@
 function solution(n, slicer, num_list) {
-    return n==1?num_list.slice(0,slicer[1]+1):n==2?num_list.slice(slicer[0],num_list.length):n==3?num_list.slice(slicer[0],slicer[1]+1):num_list.slice(slicer[0],slicer[1]+1).filter((_,i)=>i%slicer[2]==0);
+    var [a,b,c]=slicer;
+    return n==1?num_list.slice(0,b+1):n==2?num_list.slice(a):n==3?num_list.slice(a,b+1):num_list.slice(a,b+1).filter((_,i)=>i%c==0);
 }
