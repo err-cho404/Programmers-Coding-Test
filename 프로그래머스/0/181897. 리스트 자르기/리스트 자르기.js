7e807d04024d7587383f26eb7920905885c76ce7
err-cho404 <snowleo456@gmail.com>
2024-10-31 23:43:02 +0900

    [level 0] Title: 리스트 자르기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,4 +1,9 @@
-function solution(n, slicer, num_list) {
-    var [a,b,c]=slicer;
-    return n==1?num_list.slice(0,b+1):n==2?num_list.slice(a):n==3?num_list.slice(a,b+1):num_list.slice(a,b+1).filter((_,i)=>i%c==0);
+const slice={
+  1:(num_list,[a,b,c])=>num_list.slice(0,b+1),
+  2:(num_list,[a,b,c])=>num_list.slice(a),
+  3:(num_list,[a,b,c])=>num_list.slice(a,b+1),
+  4:(num_list,[a,b,c])=>num_list.slice(a,b+1).filter((_,i)=>i%c==0)
+}
+function solution(n,slicer,num_list) {
+  return slice[n](num_list,slicer);
 }
