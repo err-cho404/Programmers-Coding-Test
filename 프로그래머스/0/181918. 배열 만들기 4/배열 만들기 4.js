9bce8008308e52f1c1411db3a6366d52e9d8d332
err-cho404 <snowleo456@gmail.com>
2024-11-21 21:42:41 +0900

    [level 0] Title: 배열 만들기 4, Time: 7.05 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    var stk = [];
+    var i=0;
+    while(i<arr.length){
+        if(stk.length<1){
+            stk.push(arr[i]);
+            i+=1;
+        }else if(stk.length>0&&stk[stk.length-1]<arr[i]){
+            stk.push(arr[i]);
+            i+=1;
+        }else if(stk.length>0&&stk[stk.length-1]>=arr[i]){
+            stk.pop();
+        }
+    }
+    return stk;
+
+}
