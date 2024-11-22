092d59477e1a261c8e8a158e3a40106bcda319b8
err-cho404 <snowleo456@gmail.com>
2024-11-22 23:44:01 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/README.md
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 콜라츠 수열 만들기 - 181919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181919) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181919?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.12 ms
+메모리: 33.4 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 22일 23:41:57
+2024년 11월 22일 23:44:00
 
 ### 문제 설명
 
--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,17 +1,6 @@
-function solution(n) {
-    var x=n;
-    var answer = [];
-    while (1){
-        answer.push(x);
-        if(x%2==0){
-            x=x/2;
-        }else{
-            x=3*x+1;
-        }
-        if(x==1){
-            answer.push(x);
-            break;
-        }
-    }
-    return answer;
+function solution(n, answer = []) {
+    answer.push(n)
+    if (n === 1) return answer
+    if (n % 2 === 0) return solution(n / 2, answer)
+    return solution(3 * n + 1, answer)
 }
