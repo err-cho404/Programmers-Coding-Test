ca4c1a4f9bb97860d1cd440a2159d3d7d6e55405
err-cho404 <snowleo456@gmail.com>
2025-02-05 22:09:36 +0900

    [level 1] Title: 둘만의 암호, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.28 ms
+메모리: 33.5 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 22:08:33
+2025년 02월 05일 22:09:37
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,8 +1,5 @@
 function solution(s, skip, index) {
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     alphabet=alphabet.filter(x=>!skip.includes(x));
-    while(alphabet.length<index){
-        alphabet=alphabet.concat(alphabet);
-    }
-    return s.split('').map(x=>alphabet.indexOf(x)+index<alphabet.length?alphabet[alphabet.indexOf(x)+index]:alphabet[alphabet.indexOf(x)+index-alphabet.length]).join('');
+    return s.split('').map(x=>alphabet.indexOf(x)+index<alphabet.length?alphabet[alphabet.indexOf(x)+index]:alphabet[(alphabet.indexOf(x)+index)%alphabet.length]).join('');
 }
