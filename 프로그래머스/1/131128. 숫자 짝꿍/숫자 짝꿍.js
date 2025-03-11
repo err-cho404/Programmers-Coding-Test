f7116dd0cea5e342ab623fe01f63a79645bb0b84
err-cho404 <snowleo456@gmail.com>
2025-03-12 02:38:09 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2399.45 ms, Memory: 209 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,18 @@
+function solution(X, Y) {
+    var answer = '';
+    X = [...X].sort((a,b)=>a-b);
+    Y = [...Y].sort((a,b)=>a-b);
+    while(X.length > 0){
+        var x = X.pop();
+        while(Y.length>0 && Y[Y.length-1]>x){
+            Y.pop();
+        }
+        if(Y.length>0 && Y[Y.length-1]==x){
+            answer=answer+x;
+            Y.pop();
+        }
+    }
+    if(answer[0]=='0') answer = "0";
+    if(answer.length==0) answer = "-1";
+    return answer;
+}
