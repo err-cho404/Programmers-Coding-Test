85c6de152f541684fe3c7d62a78e62d2b0eadc35
err-cho404 <snowleo456@gmail.com>
2025-03-21 23:42:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,7 @@
 function solution(numbers) {
-    var answer = -1;
+    var answer = 0;
     for(i=0;i<10;i++){
         !numbers.includes(i)? answer += i : answer;
     }
-    return answer > -1? answer+=1 : answer;
+    return answer;
 }
