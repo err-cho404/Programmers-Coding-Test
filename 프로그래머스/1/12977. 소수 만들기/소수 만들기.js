ff5135ce890bd6ff634c0b5a347f1e45aacbbffe
err-cho404 <snowleo456@gmail.com>
2025-04-18 22:19:53 +0900

    [level 1] Title: 소수 만들기, Time: 90.38 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+    var answer = 0;
+    for(i=0;i<nums.length-2;i++){
+        for(j=i+1;j<nums.length-1;j++){
+            for(k=j+1;k<nums.length;k++){
+                var count = 0;
+                var numCheck = [nums[i],nums[j],nums[k]].reduce((a,b)=>a+b);
+                for(l=1;l<=Math.ceil(numCheck/2);l++){
+                    if(numCheck%l==0){
+                        count++;
+                    }
+                }
+                if(count==1){
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
