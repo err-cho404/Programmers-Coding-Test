532182bc4ca7b7d66130dccffedd7bd78f14e599
err-cho404 <snowleo456@gmail.com>
2025-05-02 02:55:42 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,9 @@
+function solution(strings, n) {
+    var answer = strings.sort((a,b) => {
+        if(a[n] < b[n]) return -1;
+        if(a[n] > b[n]) return 1;
+        if(a < b) return -1;
+        if(a > b) return 1;
+    });
+    return answer;
+}
