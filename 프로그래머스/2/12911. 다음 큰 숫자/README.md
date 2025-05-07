72c5bcf9d0f0f32155a1d0aa277f11f65772f949
err-cho404 <snowleo456@gmail.com>
2025-05-07 17:51:15 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/README.md
+++ 프로그래머스/2/12911. 다음 큰 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다음 큰 숫자 - 12911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12911?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.12 ms
+메모리: 33.3 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 17:48:38
+2025년 05월 07일 17:51:14
 
 ### 문제 설명
 
--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,10 +1,10 @@
 function solution(n) {
-    var nTwo = n.toString(2).split('').filter(x=>x=='1').length;
+    var nTwo = n.toString(2).match(/1/g).length;
     var answer = n;
     var answerTwo = 0;
     while(answerTwo!=nTwo){
         answer++;
-        answerTwo = answer.toString(2).split('').filter(x=>x=='1').length;
+        answerTwo = answer.toString(2).match(/1/g).length;
     }
     return answer;
 }
