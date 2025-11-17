f47ad4111818d17a557253cd6e1f8c48a1a49c60
err-cho404 <snowleo456@gmail.com>
2025-11-18 02:10:37 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,2 @@
+-- 코드를 작성해주세요
+select ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPERS where SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python') or SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#') order by ID;
