389ab1eb3fd03b395846e7debf1cda80f2b93b7f
err-cho404 <snowleo456@gmail.com>
2025-11-18 03:40:39 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 작성해주세요
+select ITEM_ID, ITEM_NAME, RARITY from ITEM_INFO where ITEM_ID in (select ITEM_ID from ITEM_TREE where PARENT_ITEM_ID in (select ITEM_ID from ITEM_INFO where RARITY = 'RARE')) order by ITEM_ID desc;
